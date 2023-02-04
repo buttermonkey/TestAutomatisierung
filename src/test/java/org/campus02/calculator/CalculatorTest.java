bc3501bdc0c6b93fda18f31c428a50f42fc535e0
Markus Stoff <s52005@edu.campus02.at>
2023-02-04 15:57:15 +0100

    Update project to use maven

--- src/test/java/org/campus02/calculator/CalculatorTest.java
+++ src/test/java/org/campus02/calculator/CalculatorTest.java
@@ -1,9 +1,8 @@
 package org.campus02.calculator;
 
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class CalculatorTest {
 
